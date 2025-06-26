dacd3d9c9dfe9e6d4eefe54fb257ebf29ca5fb6a
0thigs <machinimavlogs@gmail.com>
2025-06-25 23:24:03 -0300

    first commit

--- apps/frontend/src/components/pages/reports/most-consumed-products-and-services-table-by-pet-type/most-consumed-products-table-by-pet-type/index.tsx
+++ apps/frontend/src/components/pages/reports/most-consumed-products-and-services-table-by-pet-type/most-consumed-products-table-by-pet-type/index.tsx
@@ -1,23 +1,23 @@
 import { PetType } from '@pet-lovers/core/enums'
 
 import { ProductsTable } from '@/components/commons/products-table'
-import { useMostConsumedProductsTableByPetType } from './use-most-consumed-products-and-services-table-by-pet-type'
+import { useMostConsumedProductsTableByPetType } from './use-most-consumed-products-table-by-pet-type'
 
 type Props = {
   selectedPetType: PetType
 }
 
-export const MostConsumedProductsTableByPetType = ({ selectedPetType }: Props) =>  {
+export const MostConsumedProductsTableByPetType = ({ selectedPetType }: Props) => {
   const {
     products,
     page,
     pagesCount,
     handlePageChange,
   } = useMostConsumedProductsTableByPetType(selectedPetType)
-    
+
   return (
     <div>
-      <h2 className='mb-2 text-zinc-700 text-xl font-medium'>
+      <h2 className='mb-2 text-xl font-medium text-zinc-700'>
         Produtos mais consumidos por pet's do tipo{' '}
         {selectedPetType.toLocaleLowerCase()}
       </h2>
